dcdb94f5aa9fbbff46bd8ddc344088a7fd04034b
TinkeringWithUS <dannyzheng@utexas.edu>
2025-02-27 23:50:38 -0600

    some comments for future code

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 
 #include "fileInput.h"
 
-int main() {
+int main(int argc, char** argv) {
+  // TODO: parse arguments from argv, we want a filepath
+  // to navigate to
+  std::cout << "number of arguments: " << argc << "\n";
+
   std::cout << "Hello World!\n";
 
   FileInput fileInput = FileInput(std::string("hi")); 
 
-  std::cout << "filename: " << fileInput.getFilename() << "\n";
-
+  std::cout << "file name: " << fileInput.getFilename() << "\n";
 
   return 0;
 }
