a5c174e3eade70229949a547f3efdd17a02f6106
TinkeringWithUS <dannyzheng@utexas.edu>
2025-03-27 19:33:38 -0500

    added test data from discord

--- src/main.cpp
+++ src/main.cpp
@@ -76,6 +76,13 @@ int main(int argc, char **argv) {
   //   std::cout << globalPoints[i][2] << "]\n";
   // }
 
+	std::array<size_t,2> degrees{2,2};
+	std::array<std::vector<double>, 2> knot_vectors{std::vector<double>{1, 2, 3, 4, 5}, std::vector<double>{1, 2, 3, 4, 5}}; 
+	std::vector<std::array<double, 3>> control_points(25);
+	BSplineGeometry<2,3> b(degrees, knot_vectors, control_points);
+	std::vector<std::array<double,2>> x = {{0, 1}, {2, 3}};
+	b.evaluate(x);
+
   return 0;
 }
 
